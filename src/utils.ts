1025737ec62f8d15a512bc4548950651ca12a8d0
Pieter Scheffers <pieter.scheffers@proforto.nl>
2021-12-01 15:24:39 +0100

    feat: Add utility functions

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,7 @@
+import { URL } from 'url';
 import { backOff, IBackOffOptions } from 'exponential-backoff';
 import { JitterTypes } from 'exponential-backoff/dist/options';
+import { Client as GRPCClient, connectivityState as GRPCConnectivityState } from 'grpc';
 
 /**
  * Randomly shuffles an array.
@@ -12,12 +14,14 @@ import { JitterTypes } from 'exponential-backoff/dist/options';
  * @hidden
  */
 /* istanbul ignore next */
-export function shuffleArray(array: any[]) {
+export function shuffleArray<T>(array: T[]): T[] {
     const arrayCopy = array.slice();
+
     for (let i = arrayCopy.length - 1; i > 0; i -= 1) {
         const j = Math.floor(Math.random() * (i + 1));
         [arrayCopy[i], arrayCopy[j]] = [arrayCopy[j], arrayCopy[i]];
     }
+
     return arrayCopy;
 }
 
@@ -52,4 +56,56 @@ export function faultTolerantCall<T>(call: () => Promise<T>, retryOptions?: Part
  */
 export function constructAddress(host: string, port: number): string {
     return `${host}:${port}`;
+}
+
+/**
+ * Get a deadline timestamp
+ */
+export function getDeadline(timeout: number = 5000) {
+    return new Date().getTime() + timeout;
+}
+
+/**
+ * Get the address of a GRPC client
+ * @param client GRPC Client
+ * @returns string Address (host:port)
+ */
+export function getGrpcClientAddress(client: GRPCClient): string {
+    const channel = client.getChannel();
+    return channel.getTarget();
+}
+
+/**
+ * Get the connectionstate for a GRPC client
+ * @param client GRPC Client
+ * @param tryToConnect Boolean whether to connect when returning current state
+ * @returns GRPCConnectivityState
+ */
+export function getGrpcClientConnectivityState(client: GRPCClient, tryToConnect: boolean = false): GRPCConnectivityState {
+    try {
+        const channel = client.getChannel();
+        return channel.getConnectivityState(tryToConnect);
+    } catch (error) {
+        console.warn(`WARNING cannot get state of client connected to address ${getGrpcClientAddress(client)}. Returning state = SHUTDOWN`, error);
+        return GRPCConnectivityState.SHUTDOWN;
+    }
+}
+
+/**
+ * Make sure all addresses are fully specified URLs
+ */
+export function normalizeAddresses(address: string[] | string): string[] {
+    const addresses = Array.isArray(address) ? address : [address];
+    const fullAddresses = addresses.map(addr => {
+        const addrWithProtocol = addr.includes('://') ? addr : `http://${addr}`;
+        const url = new URL(addrWithProtocol);
+        if (!url.protocol) url.protocol = 'http';
+        if (!url.host) url.host = 'localhost';
+        if (!url.port) url.port = '9292';
+
+        return url.href;
+    });
+
+    // Remove duplicate addresses
+    return [...new Set(fullAddresses)];
 }
